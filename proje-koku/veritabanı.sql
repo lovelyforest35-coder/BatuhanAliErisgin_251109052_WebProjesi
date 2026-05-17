34d7042a2ee0d8aebea4f4d269c615a03eb9ba31
Batuhan <lovelyforest35@gmail.com>
2026-05-17 20:16:19 +0300

    Update veritabanı.sql

--- proje-koku/veritabanı.sql
+++ proje-koku/veritabanı.sql
@@ -7,12 +7,14 @@ CREATE TABLE 251109052_kullanicilar (
     b_sifre VARCHAR(100) NOT NULL,
     b_rol VARCHAR(20) DEFAULT 'ogrenci'
 );
-251109052_kullanicilar251109052_kullanicilar
+
 CREATE TABLE 251109052_kurslar (
     id INT AUTO_INCREMENT PRIMARY KEY,
     b_kurs_adi VARCHAR(100) NOT NULL,
     b_egitmen VARCHAR(50) NOT NULL,
-    b_fiyat DECIMAL(10,2) NOT NULL
+    b_fiyat DECIMAL(10,2) NOT NULL,
+    b_ekleyen_id INT DEFAULT 1,
+    FOREIGN KEY (b_ekleyen_id) REFERENCES 251109052_kullanicilar(id) ON DELETE SET NULL
 );
 
 CREATE TABLE 251109052_mesajlar (
@@ -28,7 +30,7 @@ INSERT INTO 251109052_kullanicilar (b_kullanici_adi, b_sifre, b_rol) VALUES
 ('admin', 'admin123', 'admin'),
 ('ahmet', 'ahmet123', 'ogrenci');
 
-INSERT INTO 251109052_kurslar (b_kurs_adi, b_egitmen, b_fiyat) VALUES 
-('Sıfırdan Node.js Eğitimi', 'Tuğba Hoca', 450.00),
-('Komple Web Geliştirme', 'Mehmet Can', 600.00),
-('MySQL Veritabanı Yönetimi', 'Ayşe Yılmaz', 350.00);
+INSERT INTO 251109052_kurslar (b_kurs_adi, b_egitmen, b_fiyat, b_ekleyen_id) VALUES 
+('Sıfırdan Node.js Eğitimi', 'Batuhan Ali Erişgin', 450.00, 1),
+('Komple Web Geliştirme', 'Eser Rüzgar Altunok', 600.00, 1),
+('MySQL Veritabanı Yönetimi', 'Taha Yağız', 350.00, 1);
